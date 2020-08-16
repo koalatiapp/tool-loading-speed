ad7e6c642e15b93d1fe242ef8a7ca791db7f652c
Émile Perron <contact@emileperron.com>
2020-08-16 16:21:53 -0400

    First working implementation of the tool

--- tool.js
+++ tool.js
@@ -9,32 +9,129 @@ class Tool {
     }
 
     async run() {
-        const { lighthouseResults } = await lighthouse(this.page.url(), {
-            port: (new URL(this.page.browser().wsEndpoint())).port,
-            output: 'json',
-        });
-        console.log(lighthouseResults);
+        const lighthouseResults = await this.runLighthouse();
+        this._results = this.formatLightouseResults(lighthouseResults);
+        this.sortResults();
     }
 
     get results() {
-        return [
-            {
-                'uniqueName': 'your_test_unique_name', // a test name that is unique within your tool. this will be prefixed with your tool's name to generate a Koalati-wide unique name for this test.
-                'title': 'Your test\'s user-friendly title',
-                'description': 'Your test\'s user-friendly description.', // This can be a static description of what your test looks for, or a dynamic one that describes the results.
-                'weight': 1, // the weight of this test's score as a float. the sum of the weights of all your results should be 1.0
-                'score': 1, // the score obtained as a float: 0.5 is 50%, 1.0 is 100%, etc.
-                // 'snippets': [], // a one-dimensional array of strings and/or ElementHandle that can be represented as code snippets in Koalati's results
-                // 'table': [], // a two-dimensional array of data that will be represented as a table in Koalati's results. The first row should contain the column's headings.
-                // 'recommendations': '', // a string or an array of string that gives recommendations, telling the user what can be done to improve the page
-            },
-            // ...
-        ];
-    };
+        return this._results;
+    }
 
     async cleanup() {
 
-    };
+    }
+
+    formatLightouseResults(lighthouseResults) {
+        const results = [];
+
+        for (const audit of lighthouseResults) {
+            const result = {
+                'uniqueName': audit.id,
+                'title': audit.title,
+                'description': audit.description,
+                'weight': audit.weight,
+                'score': audit.score
+            };
+
+            if (audit.score < 1) {
+                result.recommendations = audit.description;
+            }
+
+            try {
+                if (audit.details.items.length) {
+                    const table = [[]];
+
+                    for (const column of audit.details.headings) {
+                        table[0].push(column.label || '');
+                    }
+
+                    for (const rawItem of audit.details.items) {
+                        const itemRow = [];
+
+                        for (const column of audit.details.headings) {
+                            itemRow.push(this.formatSingleDetail(column, rawItem));
+                        }
+
+                        table.push(itemRow);
+                    }
+
+                    result.table = table;
+                }
+            } catch (err) { }
+
+            results.push(result);
+        }
+
+        return results;
+    }
+
+    formatSingleDetail(column, item) {
+        switch (column.valueType) {
+			case 'thumbnail':
+				return '[![](%s)](%s)'.replace(/%s/g, item[column.key]);
+
+			case 'url':
+				return '[%s](%s)'.replace(/%s/g, item[column.key]);
+
+			case 'bytes':
+				return this.formatBytes(item[column.key]);
+
+			case 'timespanMs':
+				return item[column.key] + ' ms';
+
+			default:
+				return item[column.key];
+				break;
+		}
+    }
+
+    formatBytes(bytes) {
+        if (bytes === 0) return '0 Bytes';
+
+        const k = 1024;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+    }
+
+    sortResults() {
+        this._results.sort((a, b) => {
+            if (a.weight == b.weight) {
+                return a.score > b.score ? 1 : -1;
+            }
+
+            return a.weight > b.weight ? -1 : 1;
+        });
+    }
+
+    async runLighthouse() {
+        const { lhr } = await lighthouse(this.page.url(), {
+            port: (new URL(this.page.browser().wsEndpoint())).port,
+            output: 'json',
+            onlyCategories: ['performance'],
+        });
+        const audits = [];
+
+        try {
+            for (const auditReference of lhr.categories.performance.auditRefs) {
+                try {
+                    const auditData = lhr.audits[auditReference.id];
+
+                    if (!auditData || auditData.score === null || (auditReference.group || null) != 'load-opportunities') {
+                        continue;
+                    }
+
+                    const auditReferenceCopy = JSON.parse(JSON.stringify(auditReference));
+                    audits.push(Object.assign(auditReferenceCopy, auditData));
+                } catch (err) { }
+            }
+        } catch (err) { }
+
+        return audits;
+    }
 }
 
 module.exports = Tool;
